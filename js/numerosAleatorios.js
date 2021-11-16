0053a3f9affe2c9c04e21d0dcb170ec2eea30a70
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2021-11-16 20:19:50 -0300

    cambio nombre de la función

--- js/numerosAleatorios.js
+++ js/numerosAleatorios.js
@@ -1,7 +1,7 @@
-function getRandomArbitrary(minimo, maximo) {
+function generarNumerosAleatorios(minimo, maximo) {
     return Math.floor(Math.random() * (maximo - minimo) + minimo);
   }
 
   for (let i = 1; i <= 10; ++i){
-      console.log(getRandomArbitrary(0,999));
+      console.log(generarNumerosAleatorios(0,999));
   }
